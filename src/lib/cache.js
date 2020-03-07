b26234d72eee5c2183b22e3baf744a02400eacc4
Marcio Ferreira <analistamarcio@hotmail.com>
2020-03-06 17:45:52 -0800

    Cache structure

---
+++ src/lib/cache.js
@@ -0,0 +1,27 @@
+import Redis from 'ioredis';
+
+class Cache {
+  constructor() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      keyPrefix: 'cache:',
+    });
+  }
+
+  set(key, value) {
+    return this.redis.set(key, JSON.stringify(value), 'EX', 60 * 60 * 24); // in seconds: 60seg * 60min * 24hrs
+  }
+
+  async get(key) {
+    const cached = await this.redis.get(key);
+
+    return cached ? JSON.parse(cached) : null;
+  }
+
+  invalidadate(key) {
+    return this.redis.del(key);
+  }
+}
+
+export default new Cache();
